b1e9f3f1ab11f763689175ed98bbe1da8d00e13e
jeremywbrazell <jwbrazell@gmail.com>
2021-04-27 20:46:08 -0700

    committing lab7 - 1 pass

---
+++ __tests__/basic-auth-middleware.test.js
@@ -0,0 +1,48 @@
+'use strict'
+
+require('@code-fellows/supergoose');
+const { ExpectationFailed } = require('http-errors');
+const { isMainThread } = require('worker_threads');
+const middleware = require('../src/auth/middleware/basic.js');
+const Users = require('../src/auth/models/users.js');
+
+let users = {
+    admin: { username: 'admin', password: 'password' }
+};
+
+beforeAll(async (done) => {
+    await new Users(users.admin).save();
+    done();
+});
+
+describe('Auth Middleware', () => {
+    const rew = {};
+    const res = {};
+    status: jest.fn(() => res);
+    send: jest.fn(() => res)
+})
+const next = jest.fn();
+
+describe('user authentication', () => {
+    it('fails a login for a user (admin) with the incorrect basic credentials', () => {
+        require.headers = {
+            authorization: 'Basic YWRtaW46Zm9v',
+        };
+
+        return middleware(req, res, next)
+            .then(() => {
+                expect(next).not.toHaveBeenCalled();
+                expect(res.status).toHaveBeenCalledWith(403);
+            });
+    });
+    it('logs in an admin user with the right credentials', () => {
+        req.headers = {
+            authorization: 'Basic YWRtaW46cGFzc3dvcmQ=',
+        };
+        return middleware(req, res, next)
+            .then(() => {
+                expect(next).toHaveBeenCalledWith();
+            });
+    });
+
+});
---
+++ src/auth/middleware/basic.js
@@ -0,0 +1,25 @@
+'use strict'
+
+const base64 = require('base-64');
+const User = require('../models/users.js');
+
+module.exports = async (req, res, next) => {
+
+    if (!req.headers.authorization) { next('not authorized') }
+
+    let basic = req.headers.authorization.split(' ').pop();
+    let [user, pass] = base64.decode(basic).split(':');
+
+    // try {
+    //     req.user = await User.authenticateBasic(user, pass)
+    //     next();
+    // } catch (e) {
+    //     res.status(403).send('Invalid Login');
+    // }
+    users.authenticateBasic(user, pass)
+    .then(validUser => {
+      req.user = validUser;
+      next();
+    })
+    .catch(e => next('invalid login'))
+}
